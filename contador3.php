836dacce7551e3bdefb523993dedbe4e7b62a678
NasreddineZiata <a2208059@institutmontilivi.cat>
2024-01-10 12:09:39 +0100

    MODIFICACIONS

--- contador3.php
+++ contador3.php
@@ -1,15 +1,16 @@
 <?php
 
-    $contador=0;
+$contador = 0;
 
-    if($fd = fopen('counter.txt','r')){
-        $contador=fgets($fd);
-        fclose($fd);
-    }
-    $fd2=fopen('counter.txt','w');
-    $countador=$countador+1;
-    fwrite($fd2,$contador);
-    fclose($fd2);
-    echo $contador;
+if ($fd = fopen('counter.txt', 'r')) {
+    $contador = fgets($fd);
+    fclose($fd);
+}
+
+$fd2 = fopen('counter.txt', 'w');
+$contador = $contador + 1; // Corrected variable name from $countador to $contador
+fwrite($fd2, $contador);
+fclose($fd2);
+echo $contador;
 
 ?>
